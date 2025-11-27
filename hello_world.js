264dd6e5db39e904b6b1563965226a82fa79c7a9
Rino Tovar <>
2025-11-27 10:51:03 +0100

    WDD-2 validate sum params

--- hello_world.js
+++ hello_world.js
@@ -1,8 +1,12 @@
 function sumNumbers(numbers) {
   let total = 0;
-  for (let i = 0; i < numbers.length; i += 1) {
-    total += numbers[i];
+  
+  if (numbers?.length) {
+    for (let i = 0; i < numbers.length; i += 1) {
+      total += numbers[i];
+    }
   }
+
   return total;
 }
 
